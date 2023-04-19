8cd67f40a5b54fd537ac0b3e033dbc049b80d51a
francis <francis.onyishi@spherity.com>
2023-04-19 12:01:23 +0100

    chore: clean up...

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,4 @@
  * @public
  */
 export { MyAgentPlugin } from './agent-plugin/my-plugin.js'
-export { MyKeyManagementSystem } from './key-manager/my-key-management-system.js'
-export { MyKeyStore } from './key-manager/my-key-store.js'
-export { SecretBox } from './key-manager/my-secret-box.js'
-export { MyIdentifierProvider } from './did-manager/my-identifier-provider.js'
-export { MyDIDStore } from './did-manager/my-did-store.js'
 export * from './types/IMyAgentPlugin.js'
